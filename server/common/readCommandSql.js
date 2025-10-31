65f919ac5d705275ef5d6fe631cc89c92fa628d8
cristian soares <cr.soares208@gmail.com>
2025-10-31 09:55:06 -0300

    w

--- server/common/readCommandSql.js
+++ server/common/readCommandSql.js
@@ -18,7 +18,7 @@ module.exports = class ReadCommandSql {
 
       const conteudo = fs.readFileSync(caminhoArquivo, "utf8");
 
-      // Expressão regular robusta: busca o trecho entre INIT e END em qualquer parte do arquivo
+      // Expressão regular robusta: pega tudo entre INIT e END
       const regex = new RegExp(
         `--INIT#${chave}#[\\s\\S]*?--END#${chave}#`,
         "gm"
@@ -32,7 +32,7 @@ module.exports = class ReadCommandSql {
         return null;
       }
 
-      // Remove as tags e limpa espaços
+      // Remove marcadores
       const sql = resultado[0]
         .replace(`--INIT#${chave}#`, "")
         .replace(`--END#${chave}#`, "")
